f5bb308875bd5c31c5e7b3ae72ea025745886cc8
linnene <2691234891@qq.com>
2026-04-02 12:59:13 +0800

    Begining: init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
